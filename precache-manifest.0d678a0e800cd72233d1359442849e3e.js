3cdbeb02bd11a15ebc9f82c7e37458e01bc8dbc7
Ziad Hisham Ali <ziadhesham97@gmail.com>
2019-04-18 16:35:23 +0200

    Updates

---
+++ precache-manifest.0d678a0e800cd72233d1359442849e3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3898a5fd3fd40dce9445",
+    "url": "/todoapp/static/js/runtime~main.00cdf33a.js"
+  },
+  {
+    "revision": "79dfa326f1a13ad34d00",
+    "url": "/todoapp/static/js/main.2467b49c.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/todoapp/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "79dfa326f1a13ad34d00",
+    "url": "/todoapp/static/css/main.71cfc77e.chunk.css"
+  },
+  {
+    "revision": "ba5512d27bbaffe4f187dc5b24a33c27",
+    "url": "/todoapp/index.html"
+  }
+];
