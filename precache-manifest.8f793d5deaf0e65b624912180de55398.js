d5945d5d5806e5ffed15d7c2a82264a3bdc98c30
Ziad Hisham Ali <ziadhesham97@gmail.com>
2019-04-20 10:25:21 +0200

    Updates

--- precache-manifest.8f793d5deaf0e65b624912180de55398.js
+++ precache-manifest.8f793d5deaf0e65b624912180de55398.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/todoapp/static/js/runtime~main.00cdf33a.js"
   },
   {
-    "revision": "79dfa326f1a13ad34d00",
-    "url": "/todoapp/static/js/main.2467b49c.chunk.js"
+    "revision": "608d120479cddeb9a903",
+    "url": "/todoapp/static/js/main.1b68ea6b.chunk.js"
   },
   {
     "revision": "9eee67cf782a38d35aeb",
     "url": "/todoapp/static/js/2.d6a534e5.chunk.js"
   },
   {
-    "revision": "79dfa326f1a13ad34d00",
-    "url": "/todoapp/static/css/main.71cfc77e.chunk.css"
+    "revision": "608d120479cddeb9a903",
+    "url": "/todoapp/static/css/main.4887dd3e.chunk.css"
   },
   {
-    "revision": "ba5512d27bbaffe4f187dc5b24a33c27",
+    "revision": "3e0f8bef053e3200c22bd9b9343360e2",
     "url": "/todoapp/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/todoapp/precache-manifest.0d678a0e800cd72233d1359442849e3e.js"
+  "/todoapp/precache-manifest.8f793d5deaf0e65b624912180de55398.js"
 );
 
 workbox.clientsClaim();
